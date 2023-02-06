1284ac7119334c696007b7b04eb3184321cb6eab
Antonio Alfonso González Rodríguez <alum.agonzalezr.2021@iesalixar.org>
2023-02-06 19:21:32 +0100

    añado metodos crud

--- routes/users.js
+++ routes/users.js
@@ -1,86 +1,49 @@
 var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
-var user = require("../models/User");
-// var db = mongoose.connection;
+var User = require("../models/User");
+var db = mongoose.connection;
 
 // GET del listado de usuarios
 router.get("/", function (req, res, next) {
-  res.json({
-    users: [
-      {
-        id: 123,
-        name: "Juan Perez",
-        phones: {
-          home: "999 999 888",
-          mobile: "666 555 444",
-        },
-        email: ["jp@example.com", "jp@example.org"],
-        dateOfBirth: "1980-01-02T00:00:00.000Z",
-        registered: true,
-      },
-      {
-        id: 456,
-        name: "Jose Garcia",
-        phones: {
-          home: "999 888 777",
-          mobile: "666 333 999",
-        },
-        email: ["jg@example.com ", "jg@example.org"],
-        dateOfBirth: "1983-01-09T00:00:00.000Z",
-        registered: false,
-      },
-    ],
-  });
+  User.find()
+    .sort("-creationdate")
+    .exec(function (err, users) {
+      if (err) res.status(500).send(err);
+      else res.status(200).json(users);
+    });
 });
 
 // GET de un usuario por su id
-router.get("/:id", function (req, res) {
-  if (req.params.id == "123") {
-    res.json({
-      id: 123,
-      name: "Juan Perez ",
-      phones: {
-        home: "999 999 888 ",
-        mobile: "666 555 444",
-      },
-      email: ["jp@example.com ", "jp@example.org "],
-      dateOfBirth: "1980-01-02T00:00:00.000Z",
-      registered: true,
-    });
-  } else res.status(404).send("¡Lo siento, el ítem no se ha encontrado!");
+router.get("/:id", function (req, res, next) {
+  User.findById(req.params.id, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.status(200).json(userinfo);
+  });
 });
 
 // POST de un nuevo usuario
-router.post("/", function (req, res) {
-  var new_user = req.body;
-  //ToDo (hacer algo con el nuevo usuario)
-  res
-    .status(200)
-    .send("Usuario " + req.body.name + " ha sido añadido satisfactoriamente");
+router.post("/", function (req, res, next) {
+  User.create(req.body, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
 
 // PUT de un usuario por su Id
-router.put("/:id", function (req, res) {
-  var updated_user = req.body;
-  //ToDo (hacer algo con el usuario)
-  res
-    .status(200)
-    .send(
-      "Usuario con id " +
-        req.params.id +
-        " ha sido actualizado satisfactoriamente"
-    );
+router.put("/:id", function (req, res, next) {
+  User.findByIdAndUpdate(req.params.id, req.body, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
 
 // DELETE de un usuario por su Id
-router.delete("/:id", function (req, res) {
-  //ToDo (hacer algo con el usuario)
-  res
-    .status(200)
-    .send(
-      "Usuario con id " + req.params.id + " ha sido borrado satisfactoriamente"
-    );
+router.delete("/:id", function (req, res, next) {
+  User.findByIdAndDelete(req.params.id, function (err, userinfo) {
+    if (err) res.status(500).send(err);
+    else res.sendStatus(200);
+  });
 });
 
 module.exports = router;
